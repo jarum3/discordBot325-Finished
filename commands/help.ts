b2915f1aa565593c1af5d393a6f763f0a7eeda9f
mattc101 <mcyc926@gmail.com>
2023-03-23 13:22:38 -0400

    updating help command comments

--- commands/help.ts
+++ commands/help.ts
@@ -1,9 +1,9 @@
 /**
- * Simple ping command that lists time to server, for checking that bot is running
+ * Simple help command that lists every available command
  * @packageDocumentation
  */
 import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
-// Pings, returns time pinged. A template for other commands.
+// Prints out every available command, has to be manually updated
 module.exports = {
 
   data: new SlashCommandBuilder().setName('help').setDescription('Embeds with Pong!'),
@@ -15,11 +15,11 @@ module.exports = {
       .setAuthor({ name: '325-Bot - List of Commands' })
       .addFields(
         { name: 'Add Course (/addcourse)', value: 'Adds a course to the list of courses for this server. Creates the course role with a random color, and the veteran role with a desaturated version.\nTakes parameters video, for whether the course needs a read-only channel describing how to make videos, and joint, for if the course shares a classroom with another course.' },
-        { name: 'Add Opt Role (/addoptrole)', value: 'Adds an optional role for students to join. Creates the optional role with a random color.' },
+        { name: 'Add Optional Role (/addoptrole)', value: 'Adds an optional role for students to join. Creates the optional role with a random color.' },
         { name: 'Remove Course (/removecourse)', value: 'Removes a course from the list of courses for this server. Deletes the course role, and deletes the veteran role if no users have it.' },
-        { name: 'Remove Opt Role (/removeoptrole)', value: 'Removes an optional role from the list of roles for this server. Deletes the server role.' },
+        { name: 'Remove Optional Role (/removeoptrole)', value: 'Removes an optional role from the list of roles for this server. Deletes the server role.' },
         { name: 'Select Courses Builder (/selectcoursesbuilder)', value: 'Creates a dropdown for students to select roles for courses. Use this in a channel students should access to receive roles.' },
-        { name: 'Opt Roles Builder (/optrolesbuilder)', value: 'Creates a dropdown for students to select optional roles. Use this in a channel students should access to receive roles.' },
+        { name: 'Optional Roles Builder (/optrolesbuilder)', value: 'Creates a dropdown for students to select optional roles. Use this in a channel students should access to receive roles.' },
         { name: 'Set Semester (/setsemester)', value: 'Sets the upcoming semester value, appended to each category' },
         { name: 'Start Semester (/startsemester)', value: 'Uses the currently-defined courses and optional roles to start a new semester.\nAutomatically creates channels. permissions, transfers over student roles, and archives old channels.' },
         { name: 'Create Course Category (/createcoursecategory)', value: 'Creates a category populated with appropriate channels given a course.' },
