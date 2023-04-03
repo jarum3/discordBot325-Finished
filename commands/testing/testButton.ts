6b986153d6691f65aecc62b24faa290b93688613
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-04-03 13:20:03 -0400

    Running linter

--- commands/testing/testButton.ts
+++ commands/testing/testButton.ts
@@ -1,19 +1,19 @@
 /**
- * Simple ping command that lists time to server, for checking that bot is running
+ * Shows an example of a confirmation button
  * @packageDocumentation
  */
-import { SlashCommandBuilder, ChatInputCommandInteraction, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 // Pings, returns time pinged. A template for other commands.
 module.exports = {
   data: new SlashCommandBuilder().setName('testbutton').setDescription('Replies with Pong!'),
   async execute(interaction: ChatInputCommandInteraction) {
     const row = new ActionRowBuilder<ButtonBuilder>()
-        .addComponents(
-            new ButtonBuilder()
-                .setCustomId('buttonConfirm')
-                .setLabel('Click me!')
-                .setStyle(ButtonStyle.Danger),
-        );
-    await interaction.reply({content: 'I think you should,', components: [row]});
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('buttonConfirm')
+          .setLabel('Click me!')
+          .setStyle(ButtonStyle.Danger),
+      );
+    await interaction.reply({ content: 'I think you should,', components: [row] });
   },
 };
