55de080e5a9750ebc855a62fde2d1feefb62c768
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-04-07 15:53:10 -0400

    Updating comment

--- commands/startSemester.ts
+++ commands/startSemester.ts
@@ -12,7 +12,7 @@ module.exports = {
   async execute(interaction: ChatInputCommandInteraction) {
     // Loop over each course in prevsemester.json, archive it, save that data to one file, then loop over each new course in courses.json
     // Then move each course over to prevsemester.json
-    // TODO Add confirmation button
+    // TODO Add confirmation button with embed displaying previous semesters to archive, and current semesters to instantiate
     const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
     interaction.editReply(`Pong!\nTook ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
   },
